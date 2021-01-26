1e5bc5ff778f203e95c1447e5f36fdc628ddf194
Conrad <conradorta@gmail.com>
2021-01-26 12:21:17 -0500

    files done for upgrading and seeding the database

--- app/models/card.py
+++ app/models/card.py
@@ -7,6 +7,7 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     deck = db.Column(db.String(5), nullable=False)
     name = db.Column(db.String(40), nullable=False, unique=True)
+    suit = db.Column(db.String(12), nullable=False)
     img = db.Column(db.String(255), nullable=False, unique=True)
     api_endpoint = db.Column(db.String(60), nullable=False, unique=True)
     # description = db.Column(db.Text, nullable=False)
@@ -20,6 +21,7 @@ class Card(db.Model):
             'id': self.id,
             'deck': self.deck,
             'name': self.name,
+            'suit': self.suit,
             'img': self.img,
             'api_endpoint': self.api_endpoint,
             #     'description': self.description,
