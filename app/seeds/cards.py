ccb6f3c1d4aabcf7939054bb62b47df365af879a
Conrad <conradorta@gmail.com>
2021-01-30 14:17:28 -0500

    progress is restructuring

--- app/seeds/cards.py
+++ app/seeds/cards.py
@@ -1,12 +1,12 @@
 from app.models import db, Card
 from .majorArcana import major_arcana as major
 from .minorArcana import minor_arcana as minor
-# from .description import
 
 
 def seed_cards():
     seed_major()
     seed_minor()
+    db.session.commit()
 
 
 def seed_major():
@@ -14,7 +14,7 @@ def seed_major():
                         img=card.img, api_endpoint=card.api_endpoint)
                    for card in major]
     db.session.add_all(tarot_cards)
-    db.session.commit()
+    db.session.flush()  # Sends the data to the database
 
 
 def seed_minor():
@@ -22,8 +22,13 @@ def seed_minor():
                         img=card.img, api_endpoint=card.api_endpoint)
                    for card in minor]
     db.session.add_all(tarot_cards)
+    db.session.flush()  # Sends the data to the database
+
+
+# Uses a raw SQL query to TRUNCATE the users table.
+# SQLAlchemy doesn't have a built in function to do this
+# TRUNCATE Removes all the data from the table, and resets
+# the auto incrementing primary key
+def undo_cards():
+    db.session.execute('TRUNCATE cards;')
     db.session.commit()
-
-
-def drop_all_cards():
-    db.session.drop_all()
