d0f0669d2029ae2bf8bdde6acd008182c3c4e709
Conrad <conradorta@gmail.com>
2021-02-08 19:39:23 -0500

    exporting new files

---
+++ app/seeds/card_descriptions.py
@@ -0,0 +1,5 @@
+descriptions = [
+    {
+        "text": "The World card in the tarot deck has a dancing figure at the center. The dancing figure on the card has one leg crossed over the other and holds a wand in either hand. She symbolizes balance and evolution in movement. The fulfillment and unity that she represents is not one that is static, but ever-changing, dynamic and eternal.\n\nThe green wreath of flowers that surrounds the central figure is a symbol of success, while the red ribbons that wrap around it are reminiscent of infinity. There are four figures on each corner of the card - and they are the same ones that are in the Wheel of Fortune. The four figures represent Scorpio, Leo, Aquarius and Taurus - representative of the four corners of the universe, the four elements, and the four evangelicals. Together, they symbolize the harmony between all of their energies."
+    }
+]
