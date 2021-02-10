d06c6f35dba785eaacabaac64be00250b6f69a8d
Conrad <conradorta@gmail.com>
2021-02-09 20:08:43 -0500

    Commit before new database upgrade

--- app/models/card.py
+++ app/models/card.py
@@ -5,28 +5,62 @@ class Card(db.Model):
     __tablename__ = 'cards'
 
     id = db.Column(db.Integer, primary_key=True)
-    deck = db.Column(db.String(5), nullable=False)
-    name = db.Column(db.String(40), nullable=False, unique=True)
-    suit = db.Column(db.String(12), nullable=False)
-    img = db.Column(db.String(255), nullable=False, unique=True)
-    api_endpoint = db.Column(db.String(60), nullable=False, unique=True)
-    # description = db.Column(db.Text, nullable=False)
-    # upKWs = db.Column(db.String(255), nullable=False)
-    # revKWs = db.Column(db.String(255), nullable=False)
-    # upMeaning = db.Column(db.Text, nullable=False)
-    # revMeaning = db.Column(db.Text, nullable=False)
+    name = db.Column(db.String, nullable=False, unique=True)
+    deck = db.Column(db.String, nullable=False)
+    suit = db.Column(db.String, nullable=False)
+    value = db.Column(db.Integer, nullable=False)
+    img = db.Column(db.String, nullable=False, unique=True)
+    description = db.Column(db.Text, nullable=False)
+    upright_keywords = db.Column(db.Text, nullable=False)
+    upright_general_meaning = db.Column(db.Text, nullable=False)
+    upright_genereal_quote = db.Column(db.Text, nullable=False)
+    upright_career_keywords = db.Column(db.Text, nullable=False)
+    upright_love_keywords = db.Column(db.Text, nullable=False)
+    upright_finances_keywords = db.Column(db.Text, nullable=False)
+    upright_career_meaning = db.Column(db.Text, nullable=False)
+    upright_love_meaning = db.Column(db.Text, nullable=False)
+    upright_finances_meaning = db.Column(db.Text, nullable=False)
+    reversed_keywords = db.Column(db.Text, nullable=False)
+    reversed_general_meaning = db.Column(db.Text, nullable=False)
+    reversed_genereal_quote = db.Column(db.Text, nullable=True)
+    reversed_career_keywords = db.Column(db.Text, nullable=False)
+    reversed_love_keywords = db.Column(db.Text, nullable=False)
+    reversed_finances_keywords = db.Column(db.Text, nullable=False)
+    reversed_career_meaning = db.Column(db.Text, nullable=False)
+    reversed_love_meaning = db.Column(db.Text, nullable=False)
+    reversed_finances_meaning = db.Column(db.Text, nullable=False)
+    astrology = db.Column(db.String, nullable=True)
+    element = db.Column(db.String, nullable=True)
+    zodiac = db.Column(db.String, nullable=True)
 
     def to_dict(self):
         return {
             'id': self.id,
-            'deck': self.deck,
             'name': self.name,
+            'deck': self.deck,
             'suit': self.suit,
+            'value': self.value,
             'img': self.img,
-            'api_endpoint': self.api_endpoint,
-            #     'description': self.description,
-            #     'upKWs': self.upKWs,
-            #     'revKWs': self.revKWs,
-            #     'upMeaning': self.upMeaning,
-            #     'revMeaning': self.revMeaning,
+            'description': self.description,
+            'upright_keywords': self.upright_keywords,
+            'upright_general_meaning': self.upright_general_meaning,
+            'upright_genereal_quote': self.upright_genereal_quote,
+            'upright_career_keywords': self.upright_career_keywords,
+            'upright_love_keywords': self.upright_love_keywords,
+            'upright_finances_keywords': self.upright_finances_keywords,
+            'upright_career_meaning': self.upright_career_meaning,
+            'upright_love_meaning': self.upright_love_meaning,
+            'upright_finances_meaning': self.upright_finances_meaning,
+            'reversed_keywords': self.reversed_keywords,
+            'reversed_general_meaning': self.reversed_general_meaning,
+            'reversed_genereal_quote': self.reversed_genereal_quote,
+            'reversed_career_keywords': self.reversed_career_keywords,
+            'reversed_love_keywords': self.reversed_love_keywords,
+            'reversed_finances_keywords': self.reversed_finances_keywords,
+            'reversed_career_meaning': self.reversed_career_meaning,
+            'reversed_love_meaning': self.reversed_love_meaning,
+            'reversed_finances_meaning': self.reversed_finances_meaning,
+            'astrology': self.astrology,
+            'element': self.element,
+            'zodiac': self.zodiac,
         }
--- app/seeds/cards.py
+++ app/seeds/cards.py
@@ -1,34 +1,68 @@
 from app.models import db, Card
-from .majorArcana import major_arcana as major
-from .minorArcana import minor_arcana as minor
+from app.seedables import data
+# from .majorArcana import major_arcana as major
+# from .minorArcana import minor_arcana as minor
 
 
 def seed_cards():
-    seed_major()
-    seed_minor()
+    # seed_major()
+    # seed_minor()
+    seed_deck()
     db.session.commit()
 
 
-def seed_major():
-    tarot_cards = [Card(deck=card.deck, name=card.name, suit='trump',
-                        img=card.img, api_endpoint=card.api_endpoint)
-                   for card in major]
+def seed_deck():
+    tarot_cards = [
+        Card(name=data['name'],
+             deck=data['deck'],
+             suit=['suit'],
+             value=data['value'],
+             img=data['img'],
+             description=data['description'],
+             upright_keywords=data['uprightKeywords'],
+             upright_general_meaning=data['uprightGeneralMeaning'],
+             upright_genereal_quote=data['uprightGeneralQuote'],
+             upright_career_keywords=data['uprightCareerKeywords'],
+             upright_love_keywords=data['uprightLoveKeywords'],
+             upright_finances_keywords=data['uprightFinancesKeywords'],
+             upright_career_meaning=data['uprightCareerMeaning'],
+             upright_love_meaning=data['uprightLoveMeaning'],
+             upright_finances_meaning=data['uprightFinancesMeaning'],
+             reversed_keywords=data['reversedKeywords'],
+             reversed_general_meaning=data['reversedGeneralMeaning'],
+             reversed_genereal_quote=data['reversedGeneralQuote'],
+             reversed_career_keywords=data['reversedCareerKeywords'],
+             reversed_love_keywords=data['reversedLoveKeywords'],
+             reversed_finances_keywords=data['reversedFinancesKeywords'],
+             reversed_career_meaning=data['reversedCareerMeaning'],
+             reversed_love_meaning=data['reversedLoveMeaning'],
+             reversed_finances_meaning=data['reversedFinancesMeaning'],
+             astrology=data['astrology'],
+             element=data['element'],
+             zodiac=data['zodiac'],
+             )]
     db.session.add_all(tarot_cards)
-    db.session.flush()  # Sends the data to the database
+    db.session.flush()
+    # def seed_major():
+    #     tarot_cards = [Card(deck=card.deck, name=card.name, suit='trump',
+    #                         img=card.img, api_endpoint=card.api_endpoint)
+    #                    for card in major]
+    #     db.session.add_all(tarot_cards)
+    #     db.session.flush()  # Sends the data to the database
+
+    # def seed_minor():
+    #     tarot_cards = [Card(deck=card.deck, name=card.name, suit=card.suit,
+    #                         img=card.img, api_endpoint=card.api_endpoint)
+    #                    for card in minor]
+    #     db.session.add_all(tarot_cards)
+    #     db.session.flush()  # Sends the data to the database
+
+    # Uses a raw SQL query to TRUNCATE the users table.
+    # SQLAlchemy doesn't have a built in function to do this
+    # TRUNCATE Removes all the data from the table, and resets
+    # the auto incrementing primary key
 
 
-def seed_minor():
-    tarot_cards = [Card(deck=card.deck, name=card.name, suit=card.suit,
-                        img=card.img, api_endpoint=card.api_endpoint)
-                   for card in minor]
-    db.session.add_all(tarot_cards)
-    db.session.flush()  # Sends the data to the database
-
-
-# Uses a raw SQL query to TRUNCATE the users table.
-# SQLAlchemy doesn't have a built in function to do this
-# TRUNCATE Removes all the data from the table, and resets
-# the auto incrementing primary key
 def undo_cards():
     db.session.execute('TRUNCATE cards;')
     db.session.commit()
