988e521e61df00828460909aaa78be880a0c995d
Conrad <conradorta@gmail.com>
2021-02-12 19:43:33 -0500

    added Icon to github link

--- react-app/src/components/HeroNavbar.js
+++ react-app/src/components/HeroNavbar.js
@@ -1,32 +1,33 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
 
-const HeroNavbar = ()=>{
-
-return(
-<nav className={"navbar is-primary is-fixed-top"}>
-  <div className={"container"}>
-    <div id={"navMenu"} className={"navbar-menu"}>
-      <div className={"navbar-start"}>
-        <a className={"navbar-item"}>
-          Home
-        </a>
-        <a className={"navbar-item"}>
-          Documentation
-        </a>
-      </div>
-      <div className={"navbar-end"}>
-        <div className={"navbar-item"}>
-          <div className={"buttons"}>
-            <a className={"button is-dark"}>Github</a>
-            <a className={"button is-link"}>Coming Soon!</a>
+const HeroNavbar = () => {
+  return (
+    <nav className={"navbar is-primary is-fixed-top"}>
+      <div className={"container"}>
+        <div id={"navMenu"} className={"navbar-menu"}>
+          <div className={"navbar-start"}>
+            <a className={"navbar-item"}>Home</a>
+            <a className={"navbar-item"}>Documentation</a>
+          </div>
+          <div className={"navbar-end"}>
+            <div className={"navbar-item"}>
+              <div className={"buttons"}>
+                <a className={"button is-dark"}>
+                  <span className={"icon"}>
+                    <FontAwesomeIcon icon={faGithubAlt} />
+                  </span>
+                  <span>Github</span>
+                </a>
+                <a className={"button is-link"}>Coming Soon!</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</nav>
-);
-
-}
+    </nav>
+  );
+};
 
 export default HeroNavbar;
